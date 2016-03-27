53be5c56d0a4df6d0182c3260f2bca4e251f43d9
seanbrookes <seanbrookes@beachair.ca>
2016-03-27 14:33:19 -0700

    Add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var watch = require('gulp-watch');
 var path = require('path');
 
 gulp.task('less', function () {
-  return gulp.src('./less/**/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./www/style/'));
+  return gulp.src('./client/less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('./client/www/style/'));
+});
+
+gulp.task('watch', function () {
+  watch('./client/less/**/*.less', function () {
+    gulp.start('less');
+  });
 });
